a9778c5457e0fb10b84410dc45e519382a5729ac
aluno <lvcsx@discente.ifpe.edu.br>
2024-02-22 15:34:46 -0300

    arquivos novos

---
+++ src/main/java/br/com/ifpe/oxefood/OxefoodApiLucasApplication.java
@@ -0,0 +1,13 @@
+package br.com.ifpe.oxefood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OxefoodApiLucasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OxefoodApiLucasApplication.class, args);
+	}
+
+}
